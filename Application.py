e378708bc7b30e8745a32fbe82e5e53826595d7b
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2024-06-02 17:17:48 -0400

    Implemented get_audio_features function.

--- Application.py
+++ Application.py
@@ -2,7 +2,22 @@ import json
 from dotenv import load_dotenv
 import os
 import base64
-from requests import post
+from requests import post, get
+import pandas as pd
+import altair as alt
+
+# Load selected csv data into a pandas dataframe
+
+needed_columns = ["spotify_id"]
+
+dataframe = pd.read_csv(
+    "music_preference_questionnaire_responses.csv",
+    usecols=needed_columns,
+    na_filter=False,
+    nrows=20
+)
+
+# Enable access to the Spotify Web API
 
 load_dotenv()  # Parse the .env file and load all the stored environment variables.
 
@@ -19,8 +34,23 @@ headers = {
     "Content-Type": "application/x-www-form-urlencoded"
 }
 data = {"grant_type": "client_credentials"}
-outcome = post(url, headers=headers, data=data)
-json_outcome = json.loads(outcome.content)  # The deserialization of outcome.content produces a json object
-access_token = json_outcome["access_token"]
+result = post(url, headers=headers, data=data)
+json_result = json.loads(result.content)  # The deserialization of outcome.content produces a json object
+access_token = json_result["access_token"]
 authorization_header = {"Authorization": "Bearer " + access_token}
+
+
+#  Access a selection of audio features for all tracks
+
+def get_audio_features(track_ids):
+    coordinates_dict = {}
+    for track_id in track_ids:
+        query_url = f"https://api.spotify.com/v1/audio-features/{track_id}"
+        query_result = get(query_url, headers=authorization_header)
+        json_conversion = json.loads(query_result.content)
+        coordinates_dict[track_id] = {"X": json_conversion["energy"], "Y": json_conversion["danceability"]}
+    return coordinates_dict
+
+
+coordinates_by_track_id = get_audio_features(dataframe["spotify_id"].tolist())
 
